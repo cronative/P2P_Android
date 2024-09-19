301b9b0b979528283dfc6f2db94cbc38b0455f28
Nikunj Munjiyasara <67471570+cronative@users.noreply.github.com>
2024-09-19 11:06:36 +0530

    Initial Project

---
+++ app/src/test/java/com/phace/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.phace.app
+
+import com.phace.app.appcomponents.base.BaseActivity
+import com.phace.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
